f9f8df2f612360c6a1f480d99aaf3f2936f0ef83
081dikeh <98462814+081dikeh@users.noreply.github.com>
2023-09-10 09:26:26 -0700

    Update stage1.js

--- stage1.js
+++ stage1.js
@@ -1,16 +1,41 @@
-const currentDay = document.querySelector("#currentDay");
-const currentTimeInMilliseconds = document.querySelector("#timeInMilliseconds")
+/* const currentDay = document.querySelector("#currentDay");
+ */
+const currentDay = document.querySelector('[data-testid="currentDayOfTheWeek"]') ;
+const currentTimeInMilliseconds = document.querySelector('[data-testid="currentUTCTime"]')
+const localTime = document.querySelector("#localTime")
+const hourEl = document.getElementById("hour")
+const minuteEl = document.getElementById("minute")
+const secondEl = document.getElementById("second")
+const ampmEl = document.getElementById("ampm")
 
 
 function updateTime() {
     var today = new Date();
     var day = today.getDay();
-    var milliseconds = today.getUTCMilliseconds();
+    var milliseconds = today.getTime() /* getUTCMilliseconds */;
     var dayList = ["Sunday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
+
+    if (h > 12){
+        h = h - 12;
+        ampm = "PM";
+    }
     
-    currentDay.innerHTML = dayList[day];
-    currentTimeInMilliseconds.innerHTML = "UTC Time: " + milliseconds;
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    
+    hourEl.innerText = h + " :";
+    minuteEl.innerText = m + " :";
+    secondEl.innerText = s;
+    ampmEl.innerText = ampm;
+
+    currentDay.innerHTML = "Day: " + dayList[day];
+    currentTimeInMilliseconds.innerHTML = "UTC Time(ms): </br>" + milliseconds;
 
     setTimeout(()=>{
         updateTime()
@@ -22,9 +47,4 @@ updateTime();
 
 
 
-
-
-
-
-console.log("Today is " + dayList[day] + ".");
-
+/* const currentDay = document.querySelector('[data-testid="currentUTCTime"]') ; */
